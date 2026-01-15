7d295b348b730855929d03052700b87a0c7e19dd
20225135-ui <20225135@std.neu.edu.tr>
2026-01-14 23:48:37 -0800

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'providers/cart_provider.dart';
 import 'providers/orders_provider.dart';
 import 'providers/cards_provider.dart';
 import 'screens/main_shell.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
+// ✅ Add these two screens (create files below)
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const Root());
+}
+
+class Root extends StatelessWidget {
+  const Root({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CartProvider()),
         ChangeNotifierProvider(create: (_) => OrdersProvider()),
         ChangeNotifierProvider(create: (_) => CardsProvider()),
       ],
       child: const MyApp(),
-    ),
-  );
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -32,7 +47,37 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         colorSchemeSeed: const Color(0xFF2F64FF),
       ),
-      home: const MainShell(),
+      home: const AuthGate(), // ✅ this is the “link”
+      routes: {
+        LoginScreen.routeName: (_) => const LoginScreen(),
+        SignupScreen.routeName: (_) => const SignupScreen(),
+      },
     );
   }
 }
+
+/// ✅ Decides which screen to show
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (_, snap) {
+        if (snap.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        // Logged in -> go to your existing app
+        if (snap.data != null) return const MainShell();
+
+        // Not logged in -> show login
+        return const LoginScreen();
+      },
+    );
+  }
+}
+
